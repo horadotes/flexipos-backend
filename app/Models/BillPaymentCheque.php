88c24f2e26ba1b5b177c870fd55dbd7bdb7ebe22
Heinz Kyle Alpeche Hora <hzkhora@Mac.comfast>
2024-11-06 10:07:34 +0800

    modified some columns

--- app/Models/BillPaymentCheque.php
+++ app/Models/BillPaymentCheque.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Log;
 
 class BillPaymentCheque extends Model
 {
@@ -22,6 +23,29 @@ class BillPaymentCheque extends Model
     public function billsPayment(): BelongsTo
     {
         return $this->belongsTo(BillPayment::class);
+    }
+
+    protected static function booted()
+    {
+        static::created(function ($billPaymentCheque) {
+            $bill = $billPaymentCheque->bill;
+            if ($bill) {
+                $bill->amount -= $billPaymentCheque->amount;
+                $bill->save();
+                Log::info("Amount decreased by +{$billPaymentCheque->amount} for bill ID: {$bill->id}");
+            }
+        });
+
+        static::updating(function ($billPaymentCheque) {
+            $bill = $billPaymentCheque->bill;
+            if ($bill) {
+                $originalQuantity = $billPaymentCheque->getOriginal('amount');
+                $quantityDifference = $billPaymentCheque->amount - $originalQuantity;
+                $bill->amount += $quantityDifference;
+                $bill->save();
+                Log::info("Amount adjusted by +{$quantityDifference} for Product ID: {$bill->id}");
+            }
+        });
     }
 
     // public function bank(): BelongsTo
--- app/Models/BillPaymentDetail.php
+++ app/Models/BillPaymentDetail.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Log;
 
 class BillPaymentDetail extends Model
 {
@@ -24,5 +25,28 @@ class BillPaymentDetail extends Model
     public function billsPayment(): BelongsTo
     {
         return $this->belongsTo(BillPayment::class);
+    }
+
+    protected static function booted()
+    {
+        static::created(function ($billPaymentDetail) {
+            $bill = $billPaymentDetail->bill;
+            if ($bill) {
+                $bill->amount -= $billPaymentDetail->amount;
+                $bill->save();
+                Log::info("Amount decreased by +{$billPaymentDetail->amount} for bill ID: {$bill->id}");
+            }
+        });
+
+        static::updating(function ($billPaymentDetail) {
+            $bill = $billPaymentDetail->bill;
+            if ($bill) {
+                $originalQuantity = $billPaymentDetail->getOriginal('amount');
+                $quantityDifference = $billPaymentDetail->amount - $originalQuantity;
+                $bill->amount += $quantityDifference;
+                $bill->save();
+                Log::info("Amount adjusted by +{$quantityDifference} for Product ID: {$bill->id}");
+            }
+        });
     }
 }
